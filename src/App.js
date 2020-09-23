2ea22cb8f2b7e3aa687e4a6d676206c9b5837656
luisop <luisfop77@gmail.com>
2020-09-23 12:20:24 -0300

    implemented checkbox working

--- src/App.js
+++ src/App.js
@@ -8,19 +8,23 @@ function App() {
   const usersMock = users;
 
   const [value, setValue] = useState("");
+  const [isTrue, setIsTrue] = useState(true);
 
   const valueHandler = e => {
     setValue(e.target.value);
   };
 
+  const radioHandler = e => {
+    setIsTrue(!e.target.checked);
+    // console.log(e.target.checked);
+  };
+
   return (
     <div className="App">
-      <input
-        type="text"
-        placeholder="type the name"
-        onChange={e => valueHandler(e)}
-      />
-      <Filter users={usersMock} inputValue={value} />
+      <input type="text" onChange={e => valueHandler(e)} />
+      <p>Checked?</p>
+      <input type="checkbox" onClick={e => radioHandler(e)} />
+      <Filter users={usersMock} inputValue={value} checkbox={isTrue} />
     </div>
   );
 }
--- src/components/filter.js
+++ src/components/filter.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Filter({ users, inputValue }) {
+function Filter({ users, inputValue, checkbox }) {
+  //   const [check, setChecked] = useState(Boolean);
+  //   const switchIsChecked = (userCheck, user) => {
+  //     setChecked((user.isChecked = !userCheck));
+  //   };
+
   const names = users
     .filter(user => {
-        // remove names that do not match current filter
-      return user.name.toLowerCase().indexOf(inputValue.toLowerCase()) >= 0;
+      // remove names that do not match current filter
+      return (
+        user.name.toLowerCase().indexOf(inputValue.toLowerCase()) >= 0 &&
+        user.isChecked === checkbox
+      );
     })
-    .map(user => <p className="users">{user.name}</p>);
+    .map(user => (
+      <p
+        className="users"
+        // onClick={() => switchIsChecked(user.isChecked, user)}
+      >
+        {user.name}
+      </p>
+    ));
 
   return (
     <div style={{ display: "grid", placeItems: "center" }}>
